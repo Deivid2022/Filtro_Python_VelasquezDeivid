9120e13ff4d214ac7c482e7521a9dbb2dfe283dd
Deivid <deividvelasquez122@gmail.com>
2024-02-16 09:22:55 -0500

    filtro

--- Modulo.py
+++ Modulo.py
@@ -11,13 +11,16 @@ def añadirUsuario():
     nuevo_usuario['direccion'] = input('Ingrese la direccion del usuario: ')
     nuevo_usuario['celular'] = int(input('Ingresa el numero de contacto del usuario: '))
     nuevo_usuario['año'] = int(input('Ingresa el año de cuando ingreso el usuario: '))
-    nuevo_usuario['categoria'] = Categoria(usuario)
+    nuevo_usuario['categoria'] = ''
+    
+
     usuario.append(nuevo_usuario)
     
     with open('usuarios.json','w') as archivo:
         json.dump(data,archivo,indent=4)
         
-añadirUsuario()
+#añadirUsuario()
+
 def buscarUsuario():
     with open('usuarios.json','r') as archivo:
         data = json.load(archivo)
@@ -35,7 +38,7 @@ def ActualizarUsuario():
         data = json.load(archivo)
     usuario = data['Usuario']
     id_usuario = int(input('Ingrese el numero de identidad del usuario que desea actualizar: '))
-    categoria = Categoria()
+    
     for usuari in usuario:
         if usuari['identidad'] == id_usuario:
             usuari['identidad'] = int(input('Ingresa el nuevo numero de identidad: '))
@@ -44,7 +47,7 @@ def ActualizarUsuario():
             usuari['direccion'] = input('Ingresa la nueva direccion del usuario: ')
             usuari['celular'] = int(input('Ingrese el nuevo numero de contacto: '))
             usuari['año'] = int(input('Ingrese el nuevo año de ingreso: '))
-            usuari['categoria'] = categoria
+            usuari['categoria'] = ''
     with open('usuarios.json','w') as archivo:
         json.dump(data,archivo,indent=4)
 
@@ -65,8 +68,12 @@ def eliminarUsuario():
 
 #eliminarUsuario()
 
-def Categoria(usuario):
-
+def Categoria():
+    with open('usuarios.json','r') as archivo:
+        data = json.load(archivo)
+    usuario = data['Usuario']
+    
+    
     for usuari in usuario:
         if usuari['año'] <= 2014:
             usuari['categoria'] = 'cliente Fiel'
@@ -74,8 +81,46 @@ def Categoria(usuario):
             usuari['categoria'] = 'cliente regular'
         else:
             usuari['categoria'] = 'cliente nuevo'
-    return
         
+    with open('usuarios.json','w') as archivo:
+        json.dump(data,archivo,indent=4)
+
+#Categoria()
+
+def AñadirServicio():
+    with open('servicios.json','r') as archivo:
+        data = json.load(archivo)
+    
+    servicios = data['Servicios']
+    servicios2 = input('Ingresa un servicio: ')
+    nuevo_servicios = {}
+    nuevo_servicios[servicios2] = {}
+    
+    
+    servicios.append(nuevo_servicios)
+    
+    with open('servicios.json','w') as archivo:
+        json.dump(data,archivo,indent=4)
+
+#AñadirServicio()
+
+def EliminarServicios():
+    with open('servicios.json','r') as archivo:
+        data = json.load(archivo)
+    servicios = data
+    num = input('Ingresa el nombre del servicio que deseas eliminar: ')
+    for servicio in servicios:
+        if servicio == num:
+            servicios.remove(servicio)
+    
+    with open('servicios.json','w') as archivo:
+        json.dump(data,archivo,indent=4)
+
+EliminarServicios()            
+            
+    
     
         
+
+
 
